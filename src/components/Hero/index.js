e280ce5a99b6a9a0f6ad21a52b0f37ead419f7c5
ruan ramalho <ruanmbramalho@gmail.com>
2020-09-26 15:56:38 -0300

    feat(hero): add background color and content

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './style.css';
-import Logo from '../../images/logo.jpeg';
-import { Container, Col} from 'reactstrap';
+import { Container, Col, Row} from 'reactstrap';
 
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="bg-hero"></div>
+      <Container>
+        <div className="hero-content">
+          <Col md="6" sm="12">
+            <div className="Title">
+              <h1>Não adianta correr, a limpeza precisa acontecer!</h1>
+            </div>
+            <div className="button">
+              <a href="">Contrate Já</a>
+            </div>
+          </Col>
+        </div>
+      </Container>
     </section>
   )
 }
